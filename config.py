ab1c1313687f3c08f33b9fe13ed63ce9542e2a4e
KarisAya <1048827424@qq.com>
2022-08-22 13:53:09 +0800

    3.0 修改配置项

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from pydantic import BaseModel, Extra
 class Config(BaseModel, extra=Extra.ignore):
     leaf_permission:str = "ALL"
     leaf_reply_type: int = 2
-    leaf_poke_rand:int = -1
-    leaf_repeater_limit:Tuple[int, int] = (2, 6)
-    leaf_interrupt:int = -1
+    leaf_poke_rand:int = 5
+    leaf_repeater_limit:Tuple[int, int] = (3, 6)
+    leaf_interrupt:int = 6
