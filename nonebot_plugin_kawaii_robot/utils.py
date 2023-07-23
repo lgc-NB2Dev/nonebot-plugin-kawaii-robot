86270de312b1a220f809a7f337734fd41c933028
student_2333 <lgc2333@126.com>
2023-07-23 13:37:11 +0800

    move util funcs to `utils.py`

--- nonebot_plugin_kawaii_robot/utils.py
+++ nonebot_plugin_kawaii_robot/utils.py
@@ -1,4 +1,7 @@
+import random
 from typing import List, Optional
+
+from nonebot.adapters.onebot.v11 import Bot, Message, MessageEvent
 
 from .const import NICKNAME, ReplyDictType
 
@@ -29,3 +32,68 @@ def format_vars(string: str, user_id: str, username: str, **kwargs) -> str:
         bot_nickname=NICKNAME,
         **kwargs,
     )
+
+
+def choice_reply(
+    reply_list: List[str],
+    user_id: str,
+    username: str,
+    **kwargs,
+) -> Message:
+    """
+    从提供的回复列表中随机选择一条回复并格式化
+    """
+    raw_reply = random.choice(reply_list)
+    return Message(format_vars(raw_reply, user_id, username, **kwargs))
+
+
+def format_sender_username(username: Optional[str]) -> str:
+    """
+    格式化发送者的昵称，如果昵称过长则截断
+    """
+    username = username or "你"
+    if len(username) > 10:
+        username = username[:2] + random.choice(["酱", "亲", "ちゃん", "同志", "老师"])
+    return username
+
+
+def format_username_from_event(event: MessageEvent) -> str:
+    """
+    从 MessageEvent 中获取发送者的昵称并格式化
+    """
+    return format_sender_username(event.sender.card or event.sender.nickname)
+
+
+async def get_username_by_id(bot: Bot, user_id: int, group_id: Optional[int]) -> str:
+    """
+    根据用户 id 获取用户昵称
+    """
+    if group_id:
+        info = await bot.get_group_member_info(group_id=group_id, user_id=user_id)
+        return info["card"] or info["nickname"]
+
+    info = await bot.get_stranger_info(user_id=user_id)
+    return info["nickname"]
+
+
+def transform_message(message: Message) -> Message:
+    """
+    将收到的消息中的特殊 segment（比如图片）转换为可以发送的 segment（把 `url` 改成 `file`）
+    """
+    for x in (x for x in message if (not x.is_text() and "url" in x.data)):
+        x.data["file"] = x.data.pop("url")
+    return message
+
+
+def check_percentage(need_percent: int, percentage: Optional[int] = None) -> bool:
+    """
+    检查概率
+    """
+    if need_percent <= 0:
+        return False
+    if need_percent >= 100:
+        return True
+
+    if not percentage:
+        percentage = random.randint(1, 100)
+    return percentage <= need_percent
