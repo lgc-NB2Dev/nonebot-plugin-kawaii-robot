3091b41d4c84a766d2c8e8bf0c668ee6a7ab3980
student_2333 <lgc2333@126.com>
2024-04-10 22:23:12 +0800

    4.1.0.post2

--- nonebot_plugin_kawaii_robot/__init__.py
+++ nonebot_plugin_kawaii_robot/__init__.py
@@ -7,7 +7,7 @@ require("nonebot_plugin_session")
 from . import __main__ as __main__  # noqa: E402
 from .config import ConfigModel  # noqa: E402
 
-__version__ = "4.1.0.post1"
+__version__ = "4.1.0.post2"
 __plugin_meta__ = PluginMetadata(
     name="KawaiiRobot",
     description="使用 Kyomotoi/AnimeThesaurus 的 NoneBot2 的回复（文i）插件",
@@ -15,6 +15,10 @@ __plugin_meta__ = PluginMetadata(
     type="application",
     homepage="https://github.com/KarisAya/nonebot_plugin_kawaii_robot/",
     config=ConfigModel,
-    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
+    supported_adapters=inherit_supported_adapters(
+        "nonebot_plugin_alconna",
+        "nonebot_plugin_userinfo",
+        "nonebot_plugin_session",
+    ),
     extra={},
 )
